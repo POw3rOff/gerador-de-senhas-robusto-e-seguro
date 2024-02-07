3d5dc7ed44137f9708316c74e5ae437b979ee59c
POw3rOff <141276764+POw3rOff@users.noreply.github.com>
2024-02-07 15:57:01 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-function generatePassword() {
+function generatePasswords() {
     const length = document.getElementById("length").value;
+    const includeLetters = document.getElementById("includeLetters").checked;
+    const includeNumbers = document.getElementById("includeNumbers").checked;
+    const includeSymbols = document.getElementById("includeSymbols").checked;
+    const includeAll = document.getElementById("includeAll").checked;
+
     const passwordOutput = document.getElementById("passwordOutput");
-    const password = generateRandomPassword(length);
-    passwordOutput.innerHTML = "<strong>Senha Gerada:</strong> " + password;
+    passwordOutput.innerHTML = "";
+
+    let generatedPasswords = "";
+
+    if (includeAll) {
+        const allCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+        generatedPasswords += "<p><strong>Senha com Letras:</strong> " + generateRandomPassword(length, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") + "</p>";
+        generatedPasswords += "<p><strong>Senha com Números:</strong> " + generateRandomPassword(length, "0123456789") + "</p>";
+        generatedPasswords += "<p><strong>Senha com Símbolos:</strong> " + generateRandomPassword(length, "!@#$%^&*()_+") + "</p>";
+        generatedPasswords += "<p><strong>Senha com Todas:</strong> " + generateRandomPassword(length, allCharacters) + "</p>";
+    } else {
+        if (includeLetters) {
+            generatedPasswords += "<p><strong>Senha com Letras:</strong> " + generateRandomPassword(length, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ") + "</p>";
+        }
+
+        if (includeNumbers) {
+            generatedPasswords += "<p><strong>Senha com Números:</strong> " + generateRandomPassword(length, "0123456789") + "</p>";
+        }
+
+        if (includeSymbols) {
+            generatedPasswords += "<p><strong>Senha com Símbolos:</strong> " + generateRandomPassword(length, "!@#$%^&*()_+") + "</p>";
+        }
+    }
+
+    passwordOutput.innerHTML = generatedPasswords;
 }
 
-function generateRandomPassword(length) {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+";
+function generateRandomPassword(length, charset) {
     let password = "";
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
@@ -14,3 +41,5 @@ function generateRandomPassword(length) {
     }
     return password;
 }
+
+generatePasswords(); // Chamada inicial para gerar as senhas quando a página é carregada
