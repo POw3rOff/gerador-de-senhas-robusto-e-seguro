e491bdaae97d83cdb39b82d8502427e78dfcaf40
POw3rOff <141276764+POw3rOff@users.noreply.github.com>
2024-02-07 17:16:50 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Função para gerar senhas
 function generatePasswords() {
     const length = document.getElementById("length").value;
     const includeLetters = document.getElementById("includeLetters").checked;
@@ -33,6 +34,31 @@ function generatePasswords() {
     passwordOutput.innerHTML = generatedPasswords;
 }
 
+// Função para gerar um nickname aleatório
+function generateNickname() {
+    const nicknameTypes = {
+        metin2: ["Dragão", "Guerreiro", "Samurai", "Ninja", "Monge", "Mago", "Arqueiro", "Sacerdote", "Lutador", "Necromante"],
+        trollar: ["Zueiro", "Trollador", "Gargalhada", "Brincalhão", "Risada", "Zombar", "Sorriso", "Festeiro", "Pilhéria", "Pitoresco"],
+        criminosos: ["Gângster", "Bandido", "Ladrão", "Assassino", "Fugitivo", "Contrabandista", "Traficante", "Fora da Lei", "Mercenário", "Criminoso"],
+        outros: ["Mistério", "Aventureiro", "Herói", "Viajante", "Desconhecido", "Mágico", "Lendário", "Fantasma", "Sombra", "Misterioso"]
+    };
+
+    const selectedTypes = Array.from(document.querySelectorAll('input[name="nicknameType"]:checked')).map(checkbox => checkbox.value);
+
+    if (selectedTypes.length === 0) {
+        alert("Por favor, selecione pelo menos um tipo de nickname.");
+        return;
+    }
+
+    const selectedType = selectedTypes[Math.floor(Math.random() * selectedTypes.length)];
+    const nicknames = nicknameTypes[selectedType];
+    const randomNickname = nicknames[Math.floor(Math.random() * nicknames.length)];
+
+    const nicknameOutput = document.getElementById("nicknameOutput");
+    nicknameOutput.innerHTML = "<p>Seu novo nickname é: <strong>" + randomNickname + "</strong></p>";
+}
+
+// Função para gerar uma senha aleatória com o comprimento especificado e caracteres fornecidos
 function generateRandomPassword(length, charset) {
     let password = "";
     for (let i = 0; i < length; i++) {
@@ -40,6 +66,15 @@ function generateRandomPassword(length, charset) {
         password += charset[randomIndex];
     }
     return password;
+}
+
+// Função para alternar entre os fundos preto e branco
+function toggleBackground() {
+    const body = document.querySelector("body");
+    const backgroundToggle = document.getElementById("backgroundToggle");
+    const selectedBackground = backgroundToggle.value;
+
+    body.style.backgroundColor = selectedBackground;
 }
 
 generatePasswords(); // Chamada inicial para gerar as senhas quando a página é carregada
